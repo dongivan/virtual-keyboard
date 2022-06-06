6481502fa427eb50a49acfe0d2175b7777e6cf50
Yifan Dong <588153@qq.com>
2022-06-06 09:58:59 +0800

    refactor: default config

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,34 +1,14 @@
 import { twMerge } from "tailwind-merge";
 import { VirtualKeyboardConfig, ClassName, ButtonType } from "./typings";
 import { mergeWith } from "lodash-es";
+import defaultConfig from "./defaultConfig";
 
 export function prefix(str: string) {
   return `virtual-keyboard:${str}`;
 }
 
 export function useDefaultConfig(): VirtualKeyboardConfig {
-  return {
-    defaultPageClass: "flex gap-1 flex-wrap",
-    buttonClass: {
-      btn: "w-fit min-w-[2rem] h-fit min-h-[2rem] p-2 rounded bg-gray-200",
-      active: "bg-gray-400",
-      hover: "bg-gray-300",
-      focus: "ring-2 ring-gray-100",
-      badge: [
-        "w-0 h-0 rounded-tr border-[6px] border-transparent border-t-blue-400 border-r-blue-400",
-      ],
-      slotBadge: [
-        "min-w-[50%] min-h-min scale-75 origin-top-right text-gray-600",
-      ],
-    },
-    childrenContainerClass:
-      "min-w-full w-max flex gap-1 p-1 border border-gray-400 rounded-md bg-gray-100",
-    childButtonClass: {},
-    childrenContainerOffset: {
-      alignmentAxis: 0,
-      mainAxis: 2,
-    },
-  };
+  return defaultConfig;
 }
 
 export function parseButtonType(btn: string | ButtonType): ButtonType {
