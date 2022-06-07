5c9cfdf58532a828c0c1238379e6f171b765892f
Yifan Dong <588153@qq.com>
2022-06-07 16:24:26 +0800

    chore: prepare for publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
+import * as path from "path";
 
-// https://vitejs.dev/config/
+const resolvePath = (str: string) => path.resolve(__dirname, str);
+
 export default defineConfig({
   server: {
     host: "0.0.0.0",
   },
   plugins: [vue(), eslintPlugin()],
+  build: {
+    lib: {
+      entry: resolvePath("lib/index.ts"),
+      name: "lib",
+      fileName: (format) => `lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 });
